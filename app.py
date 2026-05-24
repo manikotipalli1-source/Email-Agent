ec59a41d385e3a5e4da2965fc1f4a92e980e8a15
mani <manikotipalli1@gmail.com>
2026-05-24 21:23:44 +0530

    this version is for deployment in railway

--- app.py
+++ app.py
@@ -105,4 +105,5 @@ def activity():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
