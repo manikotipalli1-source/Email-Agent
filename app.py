8a22cd4d3ddeb3546cc36d10fdf8318b1af1f904
mani <manikotipalli1@gmail.com>
2026-05-25 16:31:52 +0530

    Fix database wipe bug and activity display

--- app.py
+++ app.py
@@ -340,7 +340,3 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
 
-# Temporary database migration
-with app.app_context():
-    db.drop_all()
-    db.create_all()
